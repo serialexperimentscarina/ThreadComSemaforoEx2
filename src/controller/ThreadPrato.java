6c0a52d730c343f8f925dca9983fa118fe74c32f
serialexperimentscarina <108838850+serialexperimentscarina@users.noreply.github.com>
2023-04-04 22:53:59 -0300

    refactor: Add comments

--- src/controller/ThreadPrato.java
+++ src/controller/ThreadPrato.java
@@ -17,9 +17,11 @@ public class ThreadPrato extends Thread {
 		String pratoNome = "";
 		
 		if (threadID % 2 == 1) {
+			// Pratos de ID ímpar, são chamados de Sopa de Cebola e levam de 0,5 a 0,8 segundos para ficar prontos.
 			pratoNome = "Sopa de Cebola";
 			cozinhar(pratoNome, 500, 800);
 		} else {
+			// Pratos de ID par, são chamados de Lasanha a Bolonhesa e levam de 0,6 a 1,2 segundos para ficar prontos. Quando
 			pratoNome = "Lasanha a Bolonesa";
 			cozinhar(pratoNome, 600, 1200);
 		}
@@ -33,6 +35,8 @@ public class ThreadPrato extends Thread {
 		}
 	}
 
+	// Quando um prato inicia, é necessário comunicar, em console, que se iniciou e, a cada 0,1 segundos, deve-se exibir
+	// o percentual de cozimento
 	private void cozinhar(String pratoNome, int tempoMinCozimento, int tempoMaxCozimento) {
 		System.out.println(pratoNome + " (ID #" + threadID + ") começou a cozinhar!");
 		
@@ -49,6 +53,9 @@ public class ThreadPrato extends Thread {
 		System.out.println(pratoNome + " (ID #" + threadID + ") terminou de cozinhar!");
 	}
 	
+	// Quando um prato fica pronto, é necessário comunicar em console o final e
+	// fazer a entrega, que leva 0,5 segundos. O jogador só pode entregar um prato por vez e
+	// deve comunicar a entrega.
 	private void entregar(String pratoNome) {
 		try {
 			sleep(500);
